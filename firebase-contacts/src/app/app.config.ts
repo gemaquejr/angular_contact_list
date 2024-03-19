81a97203d4b80bfe07343acd033ddab0ee72d753
gemaquejr <gemaquejr@hotmail.com>
2024-03-18 21:36:57 -0300

    Refatora configuracao do firebase no projeto
    Signed-off-by: gemaquejr <gemaquejr@hotmail.com>

--- firebase-contacts/src/app/app.config.ts
+++ firebase-contacts/src/app/app.config.ts
@@ -7,5 +7,19 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"contacts-56edf","appId":"1:67464451115:web:bee20ad0decd65f0c0c66e","storageBucket":"contacts-56edf.appspot.com","apiKey":"AIzaSyD3SI3MZHCpgtsczyxasUdTiUJxIOmeuXU","authDomain":"contacts-56edf.firebaseapp.com","messagingSenderId":"67464451115"}))), importProvidersFrom(provideFirestore(() => getFirestore()))]
+  providers: [
+    provideRouter(routes),
+     provideClientHydration(),
+      importProvidersFrom(
+        provideFirebaseApp(() =>
+         initializeApp({
+          "projectId":"contacts-56edf",
+          "appId":"1:67464451115:web:bee20ad0decd65f0c0c66e",
+          "storageBucket":"contacts-56edf.appspot.com",
+          "apiKey":"AIzaSyD3SI3MZHCpgtsczyxasUdTiUJxIOmeuXU",
+          "authDomain":"contacts-56edf.firebaseapp.com",
+          "messagingSenderId":"67464451115"
+        }))),
+        importProvidersFrom(provideFirestore(() => getFirestore()))
+      ],
 };
